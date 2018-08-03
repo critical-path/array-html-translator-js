707d005a5bc7fd4277afc79f70526b6d39117c29
critical-path <37241479+critical-path@users.noreply.github.com>
2018-08-03 16:50:46 -0400

    On branch master  Changes to be committed:
    	modified:   array-html-translator.js

--- array-html-translator.js
+++ array-html-translator.js
@@ -1,195 +1,195 @@
 class Translator {
-  constructor(array) {
-    this.array = array;
+    constructor(array) {
+        this.array = array
 
-    this.branchHtmlElements = [];
+        this.branchHtmlElements = []
   
-    this.voidHtmlElements = [
-      "area", 
-      "base", 
-      "br", 
-      "col", 
-      "embed", 
-      "hr", 
-      "img", 
-      "input", 
-      "keygen", 
-      "link", 
-      "meta", 
-      "param", 
-      "source", 
-      "track", 
-      "wbr"
-    ];
+        this.voidHtmlElements = [
+            "area", 
+            "base", 
+            "br", 
+            "col", 
+            "embed", 
+            "hr", 
+            "img", 
+            "input", 
+            "keygen", 
+            "link", 
+            "meta", 
+            "param", 
+            "source", 
+            "track", 
+            "wbr"
+        ]
 
-    this.htmlElementName = {
-      "delimiter": "@", 
-      "key": null, 
-      "value": ""
-    };
+        this.htmlElementName = {
+            "delimiter": "@", 
+            "key": null, 
+            "value": ""
+        }
   
-    this.htmlElementId = {
-      "delimiter": "#", 
-      "key": "id", 
-      "value": ""
-    };
+        this.htmlElementId = {
+            "delimiter": "#", 
+            "key": "id", 
+            "value": ""
+        }
 
-    this.htmlElementClass = {
-      "delimiter": ".", 
-      "key": "class", 
-      "value": ""
-    };
+        this.htmlElementClass = {
+            "delimiter": ".", 
+            "key": "class", 
+            "value": ""
+        }
 
-    this.htmlElementAttributes = {
-      "delimiter": "&", 
-      "pairs": []
-    };
+        this.htmlElementAttributes = {
+            "delimiter": "&", 
+            "pairs": []
+        }
 
-    this.htmlElementText = {
-      "delimiter": "$", 
-      "key": null, 
-      "value": ""
-    };
+        this.htmlElementText = {
+            "delimiter": "$", 
+            "key": null, 
+            "value": ""
+        }
 
-    this.htmlElementStartTag = "";
+        this.htmlElementStartTag = ""
   
-    this.htmlElementEndTag = "";
+        this.htmlElementEndTag = ""
   
-    this.htmlElement = "";
-  };
-};
+        this.htmlElement = ""
+    }
+}
 
 Translator.prototype._parseArray = function() {
-  try {
+    try {
   
-    for (let index = 0; index < this.array.length; index++) { 
-      var item = this.array[index];
+        for (let index = 0; index < this.array.length; index++) { 
+            var item = this.array[index]
 
-      if (Array.isArray(item)) { 
-        var translator = new Translator(item);
-        var branchHtmlElement = translator.translate();
-        this.branchHtmlElements.push(branchHtmlElement);
+            if (Array.isArray(item)) { 
+                var translator = new Translator(item)
+                var branchHtmlElement = translator.translate()
+                this.branchHtmlElements.push(branchHtmlElement)
 
-      } else {
-        item = item.split(" ")
+            } else {
+                item = item.split(" ")
         
-        for (let index = 0; index < item.length; index++) { 
-          var component = item[index];
+                for (let index = 0; index < item.length; index++) { 
+                    var component = item[index]
 
-          if (component.startsWith(this.htmlElementName.delimiter)) {
-            var htmlElementName = component.substring(1);
-            this.htmlElementName.value = htmlElementName;
-          };
+                    if (component.startsWith(this.htmlElementName.delimiter)) {
+                        var htmlElementName = component.substring(1)
+                        this.htmlElementName.value = htmlElementName
+                    }
             
-          if (component.startsWith(this.htmlElementId.delimiter)) {
-            var htmlElementId = component.substring(1);
-            this.htmlElementId.value = htmlElementId;
-          };
+                    if (component.startsWith(this.htmlElementId.delimiter)) {
+                        var htmlElementId = component.substring(1)
+                        this.htmlElementId.value = htmlElementId
+                    }
             
-          if (component.startsWith(this.htmlElementClass.delimiter)) {
-            var htmlElementClass = component.substring(1);
-            this.htmlElementClass.value = htmlElementClass;
-          };
+                    if (component.startsWith(this.htmlElementClass.delimiter)) {
+                        var htmlElementClass = component.substring(1)
+                        this.htmlElementClass.value = htmlElementClass
+                    }
             
-          if (component.startsWith(this.htmlElementAttributes.delimiter)) {
-            var htmlElementAttribute = component.substring(1);
-            this.htmlElementAttributes.pairs.push(htmlElementAttribute);
-          };
+                    if (component.startsWith(this.htmlElementAttributes.delimiter)) {
+                        var htmlElementAttribute = component.substring(1)
+                        this.htmlElementAttributes.pairs.push(htmlElementAttribute)
+                    }
             
-          if (component.startsWith(this.htmlElementText.delimiter)) {
-            var htmlElementText = component.substring(1);
-            this.htmlElementText.value = htmlElementText;
-          };
-        };
-      };
-    };
+                    if (component.startsWith(this.htmlElementText.delimiter)) {
+                        var htmlElementText = component.substring(1)
+                        this.htmlElementText.value = htmlElementText
+                    }
+                }
+            }
+        }
 
-  } catch(error) {
-    throw new Error("Error parsing array.");
+    } catch(error) {
+        throw new Error("Error parsing array.")
         
-  };
-};
+    }
+}
 
 Translator.prototype._getHtmlElementStartTag = function() {
-  var htmlElementStartTag = "<";
+    var htmlElementStartTag = "<"
     
-  if (this.htmlElementName.value) {
-    var htmlElementName = this.htmlElementName.value;
-    htmlElementStartTag += htmlElementName + " ";
-  };
+    if (this.htmlElementName.value) {
+        var htmlElementName = this.htmlElementName.value
+        htmlElementStartTag += htmlElementName + " "
+    }
 
-  if (this.htmlElementId.value) {
-    var htmlElementId = this.htmlElementId.key + "=" + "\"" + this.htmlElementId.value + "\"";
-    htmlElementStartTag += htmlElementId + " ";
-  };
+    if (this.htmlElementId.value) {
+        var htmlElementId = this.htmlElementId.key + "=" + "\"" + this.htmlElementId.value + "\""
+        htmlElementStartTag += htmlElementId + " "
+    }
 
-  if (this.htmlElementClass.value) {
-    var htmlElementClass = this.htmlElementClass.key + "=" + "\"" + this.htmlElementClass.value + "\"";
-    htmlElementStartTag += htmlElementClass + " ";
-  };
+    if (this.htmlElementClass.value) {
+        var htmlElementClass = this.htmlElementClass.key + "=" + "\"" + this.htmlElementClass.value + "\""
+        htmlElementStartTag += htmlElementClass + " "
+    }
     
-  if (this.htmlElementAttributes.pairs.length > 0) {
-    var htmlElementAttributes = "";
+    if (this.htmlElementAttributes.pairs.length > 0) {
+        var htmlElementAttributes = ""
     
-    for (var index = 0; index < this.htmlElementAttributes.pairs.length; index++) {
-      var attribute = this.htmlElementAttributes.pairs[index];
-      htmlElementAttributes += attribute + " "
-    };
+        for (var index = 0; index < this.htmlElementAttributes.pairs.length; index++) {
+            var attribute = this.htmlElementAttributes.pairs[index]
+            htmlElementAttributes += attribute + " "
+        }
     
-    htmlElementStartTag += htmlElementAttributes;
-  };
+        htmlElementStartTag += htmlElementAttributes
+    }
     
-  htmlElementStartTag = htmlElementStartTag.trim();
+    htmlElementStartTag = htmlElementStartTag.trim()
     
-  htmlElementStartTag += ">";
+    htmlElementStartTag += ">"
     
-  this.htmlElementStartTag = htmlElementStartTag;
-};
+    this.htmlElementStartTag = htmlElementStartTag
+}
 
 Translator.prototype._getHtmlElementEndTag = function() {    
-  if (this.voidHtmlElements.indexOf(this.htmlElementName.value) == -1) {
-    var htmlElementEndTag = "</";
+    if (this.voidHtmlElements.indexOf(this.htmlElementName.value) == -1) {
+        var htmlElementEndTag = "</"
       
-    if (this.htmlElementName.value) {
-      var htmlElementName = this.htmlElementName.value;
-      htmlElementEndTag += htmlElementName;
-    };
+        if (this.htmlElementName.value) {
+            var htmlElementName = this.htmlElementName.value
+            htmlElementEndTag += htmlElementName
+        }
       
-    htmlElementEndTag += ">";
+        htmlElementEndTag += ">"
     
-    this.htmlElementEndTag = htmlElementEndTag;
-  };
-};
+        this.htmlElementEndTag = htmlElementEndTag
+    }
+}
 
 Translator.prototype._getHtmlElement = function() {
-  var htmlElement = "";
+    var htmlElement = ""
     
-  htmlElement += this.htmlElementStartTag;
+    htmlElement += this.htmlElementStartTag
 
-  if (this.htmlElementText.value) {
-    var htmlElementText = this.htmlElementText.value;
-    htmlElement += htmlElementText
-  };
+    if (this.htmlElementText.value) {
+        var htmlElementText = this.htmlElementText.value
+        htmlElement += htmlElementText
+    }
     
-  if (this.branchHtmlElements.length > 0) {
-    for (var index = 0; index < this.branchHtmlElements.length; index++) {
-      var branchHtmlElement = this.branchHtmlElements[index];
-      htmlElement += branchHtmlElement;
-    };
-  };
+    if (this.branchHtmlElements.length > 0) {
+        for (var index = 0; index < this.branchHtmlElements.length; index++) {
+            var branchHtmlElement = this.branchHtmlElements[index]
+            htmlElement += branchHtmlElement
+        }
+    }
     
-  htmlElement += this.htmlElementEndTag;
+    htmlElement += this.htmlElementEndTag
    
-  this.htmlElement = htmlElement;
-};
+    this.htmlElement = htmlElement
+}
 
 Translator.prototype.translate = function() {
-  this._parseArray();
-  this._getHtmlElementStartTag();
-  this._getHtmlElementEndTag();
-  this._getHtmlElement();
+    this._parseArray()
+    this._getHtmlElementStartTag()
+    this._getHtmlElementEndTag()
+    this._getHtmlElement()
       
-  return this.htmlElement;
-};
+    return this.htmlElement
+}
 
-module.exports = Translator;
+module.exports = Translator
